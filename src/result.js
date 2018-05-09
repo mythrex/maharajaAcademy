b91ea1f9cdedf53289f495e158231ce86ae9267d
Mythrex <shivam.kaushik73@gmail.com>
2018-05-09 22:55:43 +0530

    Add JSON file for result

--- src/result.js
+++ src/result.js
@@ -1,4 +1,21 @@
+const charts = require('./js/charts-loader.js');
+const resultData = require('./scholarship_result.json');
+
 $(function () {
+	inits();
+})
+
+document.currentId = 0;
+
+function inits() {
 	$('.modal').modal();
 	$('select').material_select();
-})	
+}
+
+function refreshDOM(){
+	appendElements();
+}
+
+function appendElement(){
+
+}
